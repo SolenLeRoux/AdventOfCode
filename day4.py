e6d635577ddfce986b2c6a990f0e4e54f7b50b47
Solen Le Roux <solen.leroux@datadoghq.com>
2017-12-03 21:54:53 -0800

    rectify complexity because of count method

--- day4.py
+++ day4.py
@@ -19,17 +19,17 @@ TEST_SET_2 = {
 
 def is_valid(p):
     # assert there is no duplicate in the passphrase
-    # complexity: O(n) with n the number of words
+    # complexity: O(n2) with n the number of words
     words = p.split('\t' if '\t' in p else ' ')
     return [w for w in words if words.count(w) > 1] == []
 
 def solve_1(x):
-    # complexity: O(np) with p the number of passphrases
+    # complexity: O(n2 * p) with p the number of passphrases
     # and n the number of word by passphrase
     return len([p for p in x.split('\n') if is_valid(p)])
 
 def is_anagram(a, b):
-    # complexity: O(l) with l the number of letter by word
+    # complexity: O(l2) with l the number of letter by word
     if len(a) != len(b):
         return False
     for l in a:
@@ -39,7 +39,7 @@ def is_anagram(a, b):
 
 def is_valid_2(p):
     # assert there is no anagram in the passphrase
-    # complexity: O(n2 * l) with n the number of word
+    # complexity: O(n2 * l2) with n the number of word
     # and l the number of letter by word
     words = p.split('\t' if '\t' in p else ' ')
     for i in range(len(words)):
@@ -49,7 +49,7 @@ def is_valid_2(p):
     return True
 
 def solve_2(x):
-    # complexity: O(p * n2 * l) with
+    # complexity: O(p * n2 * l2) with
     # p the number of pasphrases
     # n the number of word by passphrase
     # l the number of letter by word
