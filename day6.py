e188218b384c57d11eff75e7cf7b9a1106744cad
Solen Le Roux <solen.leroux@datadoghq.com>
2017-12-09 00:36:54 -0800

    add day 6

---
+++ day6.py
@@ -0,0 +1,51 @@
+import sys
+sys.dont_write_bytecode = True
+
+TEST_SET_1 = {
+    "0  2   7   0": 5,
+}
+
+TEST_SET_2 = {
+    "0  2   7   0": 4,
+}
+
+INPUT = """0	5	10	0	11	14	13	4	11	8	8	7	1	4	12	11"""
+
+def format_input(x):
+    delimiter = '\t' if '\t' in x else ' '
+    return [int(n) for n in x.split(delimiter) if n != '']
+
+def empty_box(banks, start):
+    count = banks[start]
+    banks[start] = 0
+    for i in range(count):
+        start = start + 1 if start + 1 < len(banks) else 0
+        banks[start] += 1
+    return banks
+
+def find_box_to_empty(banks):
+    return banks.index(max(banks))
+
+def solve_1(x):
+    x = format_input(x)
+    already_seen = []
+    count = 0
+    while x not in already_seen and count < 20000:
+        already_seen.append(x[:])
+        start = find_box_to_empty(x)
+        x = empty_box(x, start)
+        count += 1
+    return count
+
+def solve_2(x):
+    x = format_input(x)
+    already_seen = []
+    count = 0
+    while x not in already_seen and count < 20000:
+        already_seen.append(x[:])
+        start = find_box_to_empty(x)
+        x = empty_box(x, start)
+        count += 1
+    start_loop = already_seen.index(x)
+    end_loop = len(already_seen)
+    return end_loop - start_loop
