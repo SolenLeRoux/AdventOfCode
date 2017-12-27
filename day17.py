0f270fb914b360829517083106366c55f3ed2bc0
Solen Le Roux <solen.leroux@datadoghq.com>
2017-12-27 17:24:04 -0500

    add day 17 part 2

--- day17.py
+++ day17.py
@@ -26,7 +26,7 @@ class Spinlock(object):
         self.buffer = first_part + [n] + second_part
         self.pos += 1
     
-    def process(self):
+    def process(self, n):
         for i in range(n):
             self.move_forward()
             self.insert_value()
@@ -38,7 +38,29 @@ def solve_1(x):
     return spinlock.buffer[next_pos]
 
 
+class AngrySpinlock(object):
+    def __init__(self, step):
+        self.step = step
+        self.pos = 0
+        self.buffer_length = 1
+        self.value = 0
+
+    def move_forward(self):
+        self.pos = (self.pos + self.step) % self.buffer_length
+
+    def insert_value(self):
+        if self.pos == 0:
+            self.value = self.buffer_length
+        self.buffer_length += 1
+        self.pos += 1
+    
+    def process(self, n):
+        for i in range(n):
+            self.move_forward()
+            self.insert_value()
+
+
 def solve_2(x):
-    spinlock = Spinlock(int(x))
+    spinlock = AngrySpinlock(int(x))
     spinlock.process(50 * 1000 * 1000)
-    return spinlock.buffer[1]
+    return spinlock.value
